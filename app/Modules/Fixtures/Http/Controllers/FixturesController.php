ae734f24d1208fdd5b28f09b3b997eddce07bb10
gogbog <gogbog11@gmail.com>
2019-06-16 11:29:26 +0300

    fixture update

--- app/Modules/Fixtures/Http/Controllers/FixturesController.php
+++ app/Modules/Fixtures/Http/Controllers/FixturesController.php
@@ -2,13 +2,22 @@
 
 namespace App\Modules\Fixtures\Http\Controllers;
 
+use App\Modules\Fixtures\Models\Fixture;
 use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
 
 class FixturesController extends Controller
 {
-    public function index() {
-        return view('fixtures::front.index');
+    public function index($slug) {
+
+        $fixture = Fixture::active()->with(['league', 'league.sport'])->where('slug', $slug)->first();
+
+        if (empty($fixture)) {
+            abort(404);
+        }
+
+
+        return view('fixtures::front.index', compact('fixture'));
     }
 }
